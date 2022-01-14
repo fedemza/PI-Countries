b2e8d35bad88d1b56ec6238cd8b8af7ae62fd9d2
Federico Voloschin <fedevoloschin@gmail.com>
2022-01-14 10:26:01 -0300

    un poco de testing

--- api/tests/models/country.spec.js
+++ api/tests/models/country.spec.js
@@ -22,3 +22,32 @@ describe('Country model', () => {
 
 });
 
+//////////
+
+describe('Activity Model', () => {
+  before(() => conn.authenticate()
+    .catch((err) => {
+      console.error('Unable to connect to the database:', err);
+    }));
+  describe('Validators', () => {
+    beforeEach(() => Activity.sync({ force: true }));
+    describe('name', () => {
+      it('should throw an error if name is null', (done) => {
+        Activity.create({})
+          .then(() => done(new Error('It requires a valid name')))
+          .catch(() => done());
+      });
+      it('should work when its a valid name', () => {
+        Activity.create({ name: 'Tenis' });
+      });
+      it('should throw an error if difficulty is other than 1, 2, 3, 4 or 5', (done) => {
+        Activity.create({ name: 'Tenis', difficulty: '6' })
+          .then(() => done(new Error('It requires a valid difficulty')))
+          .catch(() => done());
+      })
+      it('should work when its a valid difficulty', () => {
+        Activity.create({ name: 'Tenis', difficulty: '2' ,season:'Verano',countries:['Argentina','Brazil']});
+      });
+    });
+  });
+})
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,35 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+///////
+
+
+//import { Provider } from 'react-redux';
+//import Home from './components/Home';
+//import store from './store';
+import React from "react";
+import { MemoryRouter } from "react-router-dom";
+//import Detail from './components/Detail';
+
+//import axios from 'axios';
+import LandingPage from './components/LandingPage';
+// import ActivityCreate from './components/ActivityCreate';
+
+test('Landing Page has a button Ingresar', () => {
+  render(
+    <MemoryRouter>
+      <LandingPage />
+    </MemoryRouter>
+  );
+ 
+  const linkElement = screen.getByText("Ingresar");
   expect(linkElement).toBeInTheDocument();
 });
+
+
+
